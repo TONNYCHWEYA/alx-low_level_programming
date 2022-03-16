1034110a9540b8ef32239def05c3a126922a5179
TONNYCHWEYA <chweyatonny@gmail.com>
2022-03-15 17:46:18 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main -prints a sentence
+ * not allowed to use printf function
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful" -Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
