5c7f2d2a12258096e248d6d5dabceb6bb63c473d
TONNYCHWEYA <chweyatonny@gmail.com>
2022-03-22 19:17:18 -0700

    copies a number

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - copies string to anather string
+ * @dest - pointer
+ * @src - pointer
+ * Return: nothing
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+i = 0;
+while (src[i] != '\n')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest)
+}
