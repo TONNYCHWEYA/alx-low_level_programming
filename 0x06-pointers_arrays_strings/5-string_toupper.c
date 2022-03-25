b1801cabe394555a2744b805465ed4c500b1c2e0
TONNYCHWEYA <chweyatonny@gmail.com>
2022-03-24 19:44:00 -0700

    changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - change string to uppercase
+ * @str: points to string
+ * Return: nothing
+ */
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] >= 'z')
+{
+str[i] == str[i] - 32;
+}
+}
+return (str);
+}
