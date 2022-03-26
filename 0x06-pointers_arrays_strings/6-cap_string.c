d6528a70792b0b74312dbcf1d30fc44878c83b7a
TONNYCHWEYA <chweyatonny@gmail.com>
2022-03-25 18:22:45 -0700

    capitalize all words of the string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *cap_string - capitalize the first letter of each world
+ * @str: points to a string
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] ; i++)
+{
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t'
+|| str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+|| str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
+|| str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')'
+|| str[i -1] == '}' || str[i -1] == '{')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32
+}
+}
+}
+return (str);
+}
