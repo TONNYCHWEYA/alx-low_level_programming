f0661b201dcb4534240521543157058e3a207070
TONNYCHWEYA <chweyatonny@gmail.com>
2022-04-02 05:56:11 -0700

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -print the name of program
+ * @argc: number of counts
+ * @argv: points to the array of string
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+exit(EXIT_SUCCESS);
+}
